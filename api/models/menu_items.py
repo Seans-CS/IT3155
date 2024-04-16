e000dd306bc42dd62e249fa4b805902142401136
JArcigaM <143018336+JArcigaM@users.noreply.github.com>
2024-04-16 12:26:11 -0400

    Menu Items Model/schema
    Wrote the basis of the model and schema for menu items but needs to be properly connected to recipes/resources

---
+++ api/models/menu_items.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL
+from sqlalchemy.orm import relationship
+from ..dependencies.database import Base
+
+
+class MenuItems(Base):
+    __tablename__ = 'menu_items'
+
+    id = Column(Integer, primary_key=True, index=True)
+    dish_name = Column(String(100), nullable=False, index=True)
+    price = Column(DECIMAL(10, 2), nullable=False)
+    calories = Column(Integer, nullable=False)
+    food_categories = Column(String(100), nullable=False)
+
+    recipe_id = Column(Integer, ForeignKey("recipes.id"), nullable=False)
+    recipe = relationship("Recipe", back_populates="menu_item")
+
+    ingredients = relationship("Resource", secondary="recipes", back_populates="menu_items")
