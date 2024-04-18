52f1c3d07d23178f45e68f1ce0645951b352df46
william <wlampus@uncc.edu>
2024-04-18 12:17:10 -0400

    fix: updated model loader

--- api/models/model_loader.py
+++ api/models/model_loader.py
@@ -1,4 +1,4 @@
-from . import orders, order_details, recipes, sandwiches, resources
+from . import orders, order_details, recipes, sandwiches, resources, customer, customer_review, main, payment, promotions, menu_items
 
 from ..dependencies.database import engine
 
@@ -9,3 +9,10 @@ def index():
     recipes.Base.metadata.create_all(engine)
     sandwiches.Base.metadata.create_all(engine)
     resources.Base.metadata.create_all(engine)
+    customer.Base.metadata.create_all(engine)
+    customer_review.Base.metadata.create_all(engine)
+    main.Base.metadata.create_all(engine)
+    payment.Base.metadata.create_all(engine)
+    promotions.Base.metadata.create_all(engine)
+    menu_items.Base.metadata.create_all(engine)
+
